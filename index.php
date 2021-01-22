c24bbb800bdf65964e2406f4c7abb2357962d6f8
Shade-Dev <38840823+shade-sdev@users.noreply.github.com>
2021-01-22 20:43:17 +0400

    Initial Files

---
+++ index.php
@@ -0,0 +1,261 @@
+<?php
+
+$steamids = array('76561199059998077', '76561198171483802', '76561199071581505', '76561199070757820', '76561198339248076', '76561199092304776', '76561199098978987', '76561199092734032', '76561199004404047', '76561199014549844', '76561199108242032', '76561199006183965', '76561199055622022', '76561199086391128', '76561199121619531', '76561198953304603', '76561198000818067', '76561198318509289', '76561199095586506');
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html class=" responsive" lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="theme-color" content="#171a21">
+	<title>𝙋𝙊𝙀𝙎 𝘾𝙃𝙀𝘼𝙏𝙀𝙍𝙎</title>
+	<link rel="shortcut icon" href="https://steamcommunity.com/favicon.ico" type="image/x-icon">
+
+
+
+	<link href="https://community.akamai.steamstatic.com/public/shared/css/motiva_sans.css?v=GvhJzpHNW-hA&amp;l=english" rel="stylesheet" type="text/css">
+	<link href="https://community.akamai.steamstatic.com/public/shared/css/buttons.css?v=l3li_MNwxNDv&amp;l=english" rel="stylesheet" type="text/css">
+	<link href="https://community.akamai.steamstatic.com/public/shared/css/shared_global.css?v=lBL8jYejW9fi&amp;l=english" rel="stylesheet" type="text/css">
+	<link href="https://community.akamai.steamstatic.com/public/css/globalv2.css?v=K2oGiv8V598P&amp;l=english" rel="stylesheet" type="text/css">
+	<link href="https://community.akamai.steamstatic.com/public/css/globalv2.css?v=K2oGiv8V598P&amp;l=english" rel="stylesheet" type="text/css">
+	<link href="https://community.akamai.steamstatic.com/public/css/skin_1/profilev2.css?v=TqzhtMyGA0jM&amp;l=english" rel="stylesheet" type="text/css">
+	<link href="https://community.akamai.steamstatic.com/public/css/friendsv2.css?v=S8PmI9PtAP4g&amp;l=english" rel="stylesheet" type="text/css">
+	<link href="https://community.akamai.steamstatic.com/public/shared/css/shared_responsive.css?v=nZoH3ohA_bdx&amp;l=english" rel="stylesheet" type="text/css">
+	<link href="https://community.akamai.steamstatic.com/public/css/skin_1/header.css?v=Bv4kKK3Pxl5t&amp;l=english" rel="stylesheet" type="text/css">
+
+
+	<meta name="twitter:card" content="summary_large_image">
+
+	<meta name="twitter:site" content="@steam" />
+
+	<meta property="og:title" content="「𝙎𝙃𝘼𝘿𝙀」">
+	<meta property="twitter:title" content="「𝙎𝙃𝘼𝘿𝙀」">
+	<meta property="og:type" content="website">
+	<meta property="fb:app_id" content="105386699540688">
+
+
+	<link rel="image_src" href="https://community.akamai.steamstatic.com/public/shared/images/responsive/share_steam_logo.png">
+	<meta property="og:image" content="https://community.akamai.steamstatic.com/public/shared/images/responsive/share_steam_logo.png">
+	<meta name="twitter:image" content="https://community.akamai.steamstatic.com/public/shared/images/responsive/share_steam_logo.png" />
+	<meta property="og:image:secure" content="https://community.akamai.steamstatic.com/public/shared/images/responsive/share_steam_logo.png">
+
+
+
+
+</head>
+
+<body class="v6 game_bg responsive_page">
+
+	<div class="responsive_page_frame with_header">
+
+
+
+		<div class="responsive_local_menu_tab">
+
+		</div>
+
+		<div class="responsive_page_menu_ctn localmenu">
+			<div class="responsive_page_menu" id="responsive_page_local_menu">
+				<div class="localmenu_content">
+				</div>
+			</div>
+		</div>
+
+
+
+		<div class="responsive_header">
+			<div class="responsive_header_content">
+				<div id="responsive_menu_logo">
+					<img src="https://community.akamai.steamstatic.com/public/shared/images/responsive/header_menu_hamburger.png" height="100%">
+					<div class="responsive_header_notification_badge_ctn">
+						<div class="responsive_header_notification_badge notification_count_total_ctn no_notifications">
+							<span class="notification_count">0</span>
+						</div>
+					</div>
+				</div>
+				<div class="responsive_header_logo">
+					<a href="https://store.steampowered.com/">
+						<img src="https://community.akamai.steamstatic.com/public/shared/images/responsive/header_logo.png" height="36" border="0" alt="STEAM">
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="responsive_page_content_overlay">
+
+		</div>
+
+		<div class="responsive_fixonscroll_ctn nonresponsive_hidden ">
+		</div>
+
+		<div class="responsive_page_content">
+
+			<div id="global_header">
+				<div class="content">
+					<div class="logo">
+						<span id="logo_holder">
+							<a href="https://store.steampowered.com/">
+								<img src="https://community.akamai.steamstatic.com/public/shared/images/header/logo_steam.svg?t=962016" width="176" height="44">
+							</a>
+						</span>
+
+					</div>
+
+
+
+
+
+
+				</div>
+			</div>
+
+			<div class="pagecontent no_header friends">
+				<div class="friends_header_bg">
+					<div class="friends_header_container">
+						<div class="friends_header_ctn">
+							<div class="friends_header_avatar">
+								<a href="#"><img src="https://img-cdn.hltv.org/playerbodyshot/YN5S1tniupNlFh3262Qu3_.png?bg=3e4c54&h=800&ixlib=java-2.1.0&rect=101%2C23%2C490%2C490&w=800&s=1e50ecbb04d78674b9cbadc3c290c780"></a>
+							</div>
+							<div class="friends_header_name_ctn">
+								<div class="friends_header_name" style="display:inline-flex"><a href="#">𝙋𝙊𝙀𝙎 𝘾𝙃𝙀𝘼𝙏𝙀𝙍𝙎</a>
+								</div>
+							</div>
+
+
+						</div>
+					</div>
+				</div>
+
+
+
+				<div class="friends_content" id="subpage_container">
+					<div id="friends_list" class="friends_list_ctn pagecontent no_header" onload="InitFriends()">
+
+
+
+
+
+
+						<div class="profile_friends search_results" id="search_results">
+
+							<div id="search_results_empty" class="search_results_none" style="display:none">
+								Sorry, there are no friends to show.</div>
+
+							<div id="state_in-game" data-group="in-game" class="state_block">Playing</div>
+
+							<!---IN GAME-->
+
+							<?php
+
+							foreach ($steamids as $id) {
+								$file = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=0D40B3700B4C7979D5BE339A8F71F2D3&steamids=$id";
+								$data = file_get_contents($file);
+								$result = json_decode($data, true);
+
+								$status = $result['response']['players'][0]['gameid'] ?? null;
+
+								if ($status != null && $status == '730') {
+
+
+									echo '<div class="selectable friend_block_v2 persona in-game  " id="fr_106828626" data-steamid="76561198067094354" data-miniprofile="106828626" data-search="konspo ; counter-strike: global offensive">';
+									echo '';
+									echo '<div class="indicator select_friend">';
+									echo '<input class="select_friend_checkbox" type="checkbox">';
+									echo '</div>';
+									echo '';
+									echo '<a class="selectable_overlay" data-container="#fr_106828626" href="' . $result['response']['players'][0]['profileurl'] . '"></a>';
+									echo '';
+									echo '<div class="player_avatar friend_block_link_overlay in-game">';
+									echo '<img src="' . $result['response']['players'][0]['avatarmedium'] . '">';
+									echo '</div>';
+									echo '';
+									echo '<div class="friend_block_content">' . $result['response']['players'][0]['personaname'] . '<br>';
+									echo '<span class="friend_small_text">';
+									echo '<span class="friend_game_link">Counter-Strike: Global Offensive</span>';
+									echo '</span>';
+									echo '';
+									echo '</div>';
+									echo '</div>';
+								}
+							}
+							?>
+
+
+
+
+							<!--IN GAME-->
+
+							<!--NOT IN GAME-->
+							<div id="state_online" data-group="online" class="state_block">Not In Game</div>
+
+
+							<?php
+
+							foreach ($steamids as $id) {
+								$file = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=0D40B3700B4C7979D5BE339A8F71F2D3&steamids=$id";
+								$data = file_get_contents($file);
+								$result = json_decode($data, true);
+
+								$status = $result['response']['players'][0]['gameid'] ?? null;
+
+								if ($status == null) {
+
+
+									echo '<div class="selectable friend_block_v2 persona online  " id="fr_157740429" data-steamid="76561198118006157" data-miniprofile="157740429" data-search="caroed ; ">';
+									echo '';
+									echo '<div class="indicator select_friend">';
+									echo '<input class="select_friend_checkbox" type="checkbox">';
+									echo '</div>';
+									echo '';
+									echo '<a class="selectable_overlay" data-container="#fr_157740429" href="' . $result['response']['players'][0]['profileurl'] . '"></a>';
+									echo '';
+									echo '<div class="player_avatar friend_block_link_overlay online">';
+									echo '<img src="' . $result['response']['players'][0]['avatarmedium'] . '">';
+									echo '</div>';
+									echo '';
+									echo '<div class="friend_block_content">' . $result['response']['players'][0]['personaname'] . '<br>';
+									echo '<span class="friend_small_text">';
+									echo '</span>';
+									echo '';
+									echo '</div>';
+									echo '</div>';
+								}
+							}
+							?>
+
+
+
+
+
+
+							<!--NOT IN GAME-->
+
+
+
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	</div> <!-- responsive_page_legacy_content -->
+
+
+
+	</div> <!-- responsive_page_content -->
+
+	</div> <!-- responsive_page_frame -->
+</body>
+
+</html>
